073e178548c3844ba5d08c4f0e5c8a0d7c55b595
caroliinec <caroliinec.almeida@gmail.com>
2021-07-20 19:07:10 +0100

    First upload of hsports-catalog-servlet

---
+++ hsports-catalog-servlet/src/main/java/com/linkedin/servlet/CatalogServlet.java
@@ -0,0 +1,123 @@
+package com.linkedin.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CatalogServlet
+ */
+//@WebServlet(urlPatterns = "/CatalogServlet", asyncSupported = true)
+@WebServlet("/CatalogServlet")
+public class CatalogServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CatalogServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append(request.getParameter("name"));
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		String manufacturer = request.getParameter("manufacturer");
+		String sku = request.getParameter("sku");
+		
+		response.setHeader("someHeader", "someHeaderValue");
+		response.addCookie(new Cookie("someCookie", "someCookieValue"));
+		
+		Catalog.addItem(new CatalogItem(name, manufacturer, sku));
+		
+		request.setAttribute("items", Catalog.getItems());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("list.jsp");
+		dispatcher.forward(request, response);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	/*protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		AsyncContext asyncContext = request.startAsync();
+		
+		asyncContext.start(new Runnable() {
+			
+			@Override
+			public void run() {				
+				try {
+					Thread.sleep(5000);
+					System.out.println("Print the response");
+					System.out.println("Response returned by: " + Thread.currentThread().getName());
+					returnResponse(request, response);
+					asyncContext.complete();
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		System.out.println("Initial request: " + Thread.currentThread().getName());
+	}
+
+	private void returnResponse(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String name = request.getParameter("name");
+		String manufacturer = request.getParameter("manufacturer");
+		String sku = request.getParameter("sku");
+		
+		Catalog.addItem(new CatalogItem(name, manufacturer, sku));
+		
+		response.setHeader("someHeader", "someHeaderValue");
+		response.addCookie(new Cookie("someCookie", "someCookieValue"));
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html>");
+		out.println("<head></head>");
+		out.println("<body>");
+		out.println("<table>");
+		
+		for(CatalogItem item : Catalog.getItems()) {
+			out.println("<tr>");
+			out.println("<td>");
+			out.println(item.getName());
+			out.println("</td>");
+			out.println("<td>");
+			out.println(item.getManufacturer());
+			out.println("</td>");
+			out.println("<td>");
+			out.println(item.getSku());
+			out.println("</td>");
+			out.println("</tr>");
+		}
+		
+		out.println("</table>");
+		out.println("</body>");
+		out.println("</html>");
+	}*/
+
+}
